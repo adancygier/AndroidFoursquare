132726fe0b30e84886f96ef62af63960a3d15ec0
Aaron Dancygier <aaron@dancygier.com>
2012-01-16 17:49:24 -0500

    Reimplementing getVenues(), converting project to library and adding new container type for venues.

--- src/net/londatiga/fsq/FoursquareApp.java
+++ src/net/londatiga/fsq/FoursquareApp.java
@@ -1,28 +1,28 @@
 package net.londatiga.fsq;
 
-import net.londatiga.fsq.FoursquareDialog.FsqDialogListener;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import net.londatiga.fsq.FoursquareDialog.FsqDialogListener;
+
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-import android.content.Context;
-import android.util.Log;
 import android.app.ProgressDialog;
-
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 
 /**
  * 
@@ -74,6 +74,7 @@ public class FoursquareApp {
 		
 		mProgress.setCancelable(false);
 	}
+    
 	
 	private void getAccessToken(final String code) {
 		mProgress.setMessage("Getting access token ...");
@@ -181,6 +182,10 @@ public class FoursquareApp {
 	public boolean hasAccessToken() {
 		return (mAccessToken == null) ? false : true;
 	}
+    
+    public void logOut() {
+    	this.mSession.resetAccessToken();
+    }
 	
 	public void setListener(FsqAuthListener listener) {
 		mListener = listener;
@@ -192,6 +197,135 @@ public class FoursquareApp {
 	
 	public void authorize() {
 		mDialog.show();
+	}
+    
+	public ArrayList<FoursquareVenue> getVenues(String lat, String lon, String query) throws MalformedURLException, IOException, JSONException {
+		ArrayList<FoursquareVenue> venueList = new ArrayList<FoursquareVenue>();
+        
+		String ll 	= lat + "," + lon;
+        String url_str = API_URL + "/venues/search?ll=" + ll + "&oauth_token=" + mAccessToken;
+            
+        if (query != null && query.length() > 0) {
+        	String query_encoded = java.net.URLEncoder.encode(query);
+    		query_encoded = query_encoded.replace("+", "%20");
+        	url_str = url_str + "&query=" + query_encoded;
+        }
+            
+		URL url;
+		try {
+			url = new URL(url_str);
+		    Log.d(TAG, "Opening URL " + url.toString());
+			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+			urlConnection.setRequestMethod("GET");
+			urlConnection.setDoInput(true);
+			urlConnection.setDoOutput(true);
+			urlConnection.connect();
+			String response		= streamToString(urlConnection.getInputStream());
+                
+    			
+            JSONObject jsonObj = (JSONObject) new JSONTokener(response).nextValue();
+			JSONArray groups = (JSONArray) jsonObj.getJSONObject("response").getJSONArray("groups");
+    		
+			int length = groups.length();
+                
+    			
+			if (length > 0) {
+                for (int i = 0; i < groups.length(); i ++) {
+    				JSONObject group = (JSONObject) groups.get(i);
+    				JSONArray items = (JSONArray) group.getJSONArray("items");
+        					
+    				for (int j = 0; j < items.length(); j++) {
+    					JSONObject item = (JSONObject) items.get(j);
+        						
+    					FoursquareVenue venue = new FoursquareVenue();
+    					venue.id 		= item.getString("id");
+    					venue.name		= item.getString("name");
+        						
+    					JSONObject location = (JSONObject) item.getJSONObject("location");
+    					JSONArray categories= (JSONArray) item.getJSONArray("categories");
+                            
+                        //Log.v(TAG, "location: " +  location.toString());
+                        //Log.v(TAG, "categories: " +  categories.toString());
+                            
+    					if (categories.length() > 0) {
+                            JSONObject cat = (JSONObject) categories.get(0);
+                                
+                            if (cat.has("icon")) {
+                                venue.icon = cat.getString("icon");
+                            }
+                            else {
+                                venue.icon = "";	
+                            }
+    				    }
+    					else {
+    						venue.icon = "";
+    					}
+        						
+                        if (location.has("address")) {
+    					    venue.address = location.getString("address");
+                        }
+                        else {
+                        	venue.address = "";
+                        }
+                            
+                        if (location.has("distance")) {
+    					    venue.distance = location.getInt("distance");
+                        }
+                        else {
+                        	venue.distance = 0;
+                        }
+                            
+                        if (group.has("type")) {
+    						venue.type = group.getString("type");
+                        }
+                        else {
+                        	venue.type = "";
+                        }
+                            
+                        if (location.has("city")) {
+                            venue.city = location.getString("city");
+                        }
+                        else {
+                            venue.city = "";
+                        }
+                            
+                        if (location.has("state")) {
+                            venue.state = location.getString("state");
+                        }
+                        else {
+                            venue.state = "";
+                        }
+                            
+                        if (location.has("postalCode")) {
+                            venue.postalCode = location.getString("postalCode");
+                        }
+                        else {
+                            venue.postalCode = "";
+                        }
+                            
+                        if (item.has("hereNow") && item.getJSONObject("hereNow").has("count")) {
+    					    venue.herenow = item.getJSONObject("hereNow").getInt("count");
+                        }
+                        else {
+                            venue.herenow = 0;	
+                        }
+                                
+    					venueList.add(venue);
+    				}
+                }
+		    }
+		} catch (MalformedURLException mfe) {
+            Log.e(TAG, "Caught MalformedURLException in venues api call: " + mfe.toString());
+			throw mfe;
+		} catch (IOException ioe) {
+            Log.e(TAG, "Caught IOException in venues api call: " + ioe.toString());
+            throw ioe;
+		} catch (JSONException jsone) {
+            Log.e(TAG, "Caught JSONException in venues api call: " + jsone.toString());
+            throw jsone;
+		}
+			
+		return venueList;
 	}
 	
 	public ArrayList<FsqVenue> getNearby(double latitude, double longitude) throws Exception {
---
+++ src/net/londatiga/fsq/FoursquareVenue.java
@@ -0,0 +1,19 @@
+package net.londatiga.fsq;
+
+import android.location.Location;
+
+public class FoursquareVenue {
+    
+	public String id;
+	public String name;
+	public String address;
+	public String type;
+	public Location location;
+	public int direction;
+	public int distance;
+    public String postalCode;
+    public String city;
+    public String state;
+    public String icon;
+	public int herenow;
+}
